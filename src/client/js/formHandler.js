f8b71c2387de88dbced9806d473aec2e123ab196
concept2work <48356445+concept2work@users.noreply.github.com>
2021-03-26 20:53:44 +0100

    removed obsolete functions

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -96,124 +96,6 @@ const unhighlightInput = () => {
 };
 
 /*
-  The weather data is received from the API. The passed URI must include all needed parameters.
-  If the call is successful a JSON object is populated from the API response and returned.
-*/
-// const getWeather = async (uri) => {
-//   const res = await fetch(uri);
-//   try {
-//     // If the API sends OK, weather data is returned.
-//     if (res.ok) {
-//       /*
-//         The success notification is sent to give additional feedback to the user.
-//         UIkit (https://getuikit.com) notification is used for feedback purposes.
-//       */
-//       UIkit.notification('Success: city found', { status: 'success' });
-//       /*
-//         If a invalid zip code is getting valid via language change, unhighlightInput()
-//         ensures that the highlighted input is removed.
-//       */
-//       unhighlightInput();
-//       const weatherData = await res.json();
-//       return weatherData;
-//     }
-//     /*
-//       In any other case an error message is displayed to the user.
-//       The error message is retrieved from the API to provide a meaningful
-//       error message to the user.
-//     */
-//     if (!res.ok) {
-//       const errorData = await res.json();
-//       const input = getZipCode();
-//       // UIkit (https://getuikit.com) notification is used for feedback purposes.
-//       UIkit.notification(`Error: ${errorData.message}`, { status: 'danger' });
-//       /*
-//         Special case: if the city is not found or the zip code is too short,
-//         the user is guided to the zip input field to correct his/her input.
-//       */
-//       if (res.status === 404 || res.status === 400) {
-//         input.focus();
-//         highlightInput();
-//         document.getElementById('zip').addEventListener('keydown', () => {
-//           unhighlightInput();
-//           UIkit.notification.closeAll();
-//         });
-//       }
-//     }
-//   } catch (error) {
-//     console.error('the following error occured: ', error.message);
-//   }
-//   return null;
-// };
-
-/*
-  The updateUI function initializes/updates given user data and API received
-  weather data on the UI.
-*/
-const updateUI = async () => {
-  // The user and weather data from the getProjectData route is received.
-  const res = await fetch(queryLocalServer('/api/getProjectData'));
-  try {
-    const weatherData = await res.json();
-    // const heading = document.getElementById('journal');
-    // if (heading !== null) {
-    //   heading.removeChild(heading.childNodes[0]);
-    // }
-    // The elements are generated or updated to show the weather data and user feelings
-    // document.getElementById('entry-holder').style.cssText = 'display: initial;';
-    // heading.insertAdjacentHTML('afterBegin', '<h2 class="uk-heading-line uk-text-center uk-margin-top"><span>Most Recent Entry</span></h2>');
-    // document.getElementById('name').innerHTML = `${weatherData.name}, ${weatherData.country}`;
-    // document.getElementById('icon').innerHTML = `<i class="owi owi-${weatherData.icon}"></i>`;
-    // document.getElementById('date').innerHTML = weatherData.date;
-    // document.getElementById('temp').innerHTML = `${weatherData.temperature} °C`;
-    // If a feeling is entered in the text area, the element is generated.
-    // if (weatherData.feelings !== '') {
-    //   document.getElementById('entry-holder').insertAdjacentHTML('beforeEnd', '<div id="content" class="uk-text-center"></div>');
-    //   document.getElementById('content').innerHTML = `Today I feel: ${weatherData.feelings}`;
-    // }
-    // // If there is no user input in the field, the element is removed.
-    // if (weatherData.feelings === '') {
-    //   if (document.getElementById('content')) {
-    //     document.getElementById('content').remove();
-    //   }
-    // }
-  } catch (error) {
-    console.error('the following error occured: ', error.message);
-  }
-};
-
-/*
-  The Open Weather Map API is queried. The user input for the zip code and the country code is used.
-  If there is no country code given the call is made without it: in this case the default country
-  for the zip code search is USA.
-*/
-// const queryWeatherAPI = () => {
-//   // The URI for the API call is put together
-//   const weatherServiceApiCallUri = `https://${weatherServiceUri}zip=${getZipCode().value}${selectedCountry}&appid=${apiKey}&units=metric`;
-//   console.log(weatherServiceApiCallUri);
-//   // The function for the GET request with the assembled URI is called.
-//   getWeather(weatherServiceApiCallUri)
-//     /*
-//       After the weather data is returned the postWeather route is used to pass the information
-//       to the server.
-//     */
-//     .then((weatherData) => {
-//       if (weatherData) {
-//         postWeather('/api/postWeather', {
-//           icon: weatherData.weather[0].icon,
-//           name: weatherData.name,
-//           country: weatherData.sys.country,
-//           temperature: weatherData.main.temp,
-//           date: `${currentDate.toDateString()}, ${currentDate.toLocaleTimeString('en-US')}`,
-//           feelings: getFeelings(),
-//         });
-//         // After the server received the updated information, the UI is updated.
-//         updateUI();
-//       }
-//     });
-// };
-
-/*
   The language selector is populated by the values  of the languages object
   (key: country code, value: country name). The languages are retrieved via the server,
   which uses the i18n-iso-countries module to retrieve language codes and their respective names.
@@ -255,19 +137,50 @@ const getInput = () => {
   The postUserData function specifies a POST request in which a data object is passed in the
   request body.
 */
-const postUserSelection = async (uri, data = {}) => {
-  console.log(uri);
-  console.log(`data.countryCode: ${data.countryCode}`);
-  await fetch(uri, {
+// const postUserSelection = async (uri, data = {}) => {
+//   console.log(uri);
+//   console.log(`data.countryCode: ${data.countryCode}`);
+//   await fetch(uri, {
+//     method: 'POST',
+//     credentials: 'same-origin',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(data),
+//   }).catch((error) => {
+//     console.error('the following error occured: ', error.message);
+//   });
+// };
+
+const getProjectData = async () => {
+  const res = await fetch(queryLocalServer('/api/getProjectData'));
+  try {
+    const response = await res.json();
+    console.log(response);
+  } catch (error) {
+    console.error('the following error occured: ', error.message);
+  }
+};
+
+const postUserSelection = async () => {
+  const settings = {
     method: 'POST',
     credentials: 'same-origin',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(data),
-  }).catch((error) => {
+    body: JSON.stringify(getInput()),
+  };
+  try {
+    const req = await fetch(queryLocalServer('/api/postUserSelection'), settings);
+    // const data = await res.json();
+    if (req.ok) {
+      console.log(req.status);
+    }
+  } catch (error) {
     console.error('the following error occured: ', error.message);
-  });
+    // return e;
+  }
 };
 
 /*
@@ -276,7 +189,9 @@ const postUserSelection = async (uri, data = {}) => {
 */
 const submitInfo = () => {
   console.log('Button clicked.');
-  postUserSelection(queryLocalServer('/api/postUserSelection'), getInput());
+  postUserSelection().then(
+    getProjectData(),
+  );
 };
 
 /*
@@ -286,7 +201,9 @@ const submitInfo = () => {
 document.getElementById('city').addEventListener('keypress', (event) => {
   if (event.key === 'Enter') {
     console.log('Enter pressed.');
-    postUserSelection(queryLocalServer('/api/postUserSelection'), getInput());
+    postUserSelection().then(
+      getProjectData(),
+    );
   }
 });
 
@@ -303,4 +220,3 @@ window.addEventListener('load', (event) => {
 
 export { submitInfo };
 export { inputCountry };
-export { updateUI };
